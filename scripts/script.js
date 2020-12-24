95b61c510384160e43a532fd344c28a96e942342
qpongratz <lagarathan@gmail.com>
2020-12-24 15:49:49 -0600

    Add basic math functions

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+//Math Functions
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
